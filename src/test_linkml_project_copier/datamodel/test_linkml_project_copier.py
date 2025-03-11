20883e521cca9d67ae2fd4929fec02d878f831b9
David Linke <dr.david.linke@gmail.com>
2025-03-11 22:00:45 +0100

    Update template to 0.3.0-post2-gf9ff47b

--- src/test_linkml_project_copier/datamodel/test_linkml_project_copier.py
+++ src/test_linkml_project_copier/datamodel/test_linkml_project_copier.py
@@ -1,5 +1,5 @@
 # Auto generated from test_linkml_project_copier.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-02-10T22:46:28
+# Generation date: 2025-03-11T22:12:29
 # Schema: test-linkml-project-copier
 #
 # id: https://w3id.org/dalito/test-linkml-project-copier
@@ -9,55 +9,19 @@
 import dataclasses
 import re
 from dataclasses import dataclass
-from datetime import (
-    date,
-    datetime,
-    time
-)
-from typing import (
-    Any,
-    ClassVar,
-    Dict,
-    List,
-    Optional,
-    Union
-)
+from typing import Any, ClassVar, Dict, List, Optional, Union
 
-from jsonasobj2 import (
-    JsonObj,
-    as_dict
-)
-from linkml_runtime.linkml_model.meta import (
-    EnumDefinition,
-    PermissibleValue,
-    PvFormulaOptions
-)
+from linkml_runtime.linkml_model.meta import EnumDefinition, PermissibleValue
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.utils.dataclass_extensions_376 import dataclasses_init_fn_with_kwargs
+from linkml_runtime.utils.dataclass_extensions_376 import (
+    dataclasses_init_fn_with_kwargs,
+)
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
-from linkml_runtime.utils.formatutils import (
-    camelcase,
-    sfx,
-    underscore
-)
-from linkml_runtime.utils.metamodelcore import (
-    bnode,
-    empty_dict,
-    empty_list
-)
+from linkml_runtime.utils.metamodelcore import empty_dict
 from linkml_runtime.utils.slot import Slot
-from linkml_runtime.utils.yamlutils import (
-    YAMLRoot,
-    extended_float,
-    extended_int,
-    extended_str
-)
-from rdflib import (
-    Namespace,
-    URIRef
-)
+from linkml_runtime.utils.yamlutils import YAMLRoot
+from rdflib import URIRef
 
-from linkml_runtime.linkml_model.types import Date, Integer, String, Uriorcurie
 from linkml_runtime.utils.metamodelcore import URIorCURIE, XSDDate
 
 metamodel_version = "1.7.0"
@@ -67,16 +31,19 @@ version = None
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
-PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
-BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/')
-EXAMPLE = CurieNamespace('example', 'https://example.org/')
-LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
-SCHEMA = CurieNamespace('schema', 'http://schema.org/')
-TEST_LINKML_PROJECT_COPIER = CurieNamespace('test_linkml_project_copier', 'https://w3id.org/dalito/test-linkml-project-copier/')
+PATO = CurieNamespace("PATO", "http://purl.obolibrary.org/obo/PATO_")
+BIOLINK = CurieNamespace("biolink", "https://w3id.org/biolink/")
+EXAMPLE = CurieNamespace("example", "https://example.org/")
+LINKML = CurieNamespace("linkml", "https://w3id.org/linkml/")
+SCHEMA = CurieNamespace("schema", "http://schema.org/")
+TEST_LINKML_PROJECT_COPIER = CurieNamespace(
+    "test_linkml_project_copier", "https://w3id.org/dalito/test-linkml-project-copier/"
+)
 DEFAULT_ = TEST_LINKML_PROJECT_COPIER
 
 
 # Types
+
 
 # Class references
 class NamedThingId(URIorCURIE):
@@ -92,6 +59,7 @@ class NamedThing(YAMLRoot):
     """
     A generic grouping for any identifiable entity
     """
+
     _inherited_slots: ClassVar[List[str]] = []
 
     class_class_uri: ClassVar[URIRef] = SCHEMA["Thing"]
@@ -123,6 +91,7 @@ class Person(NamedThing):
     """
     Represents a Person
     """
+
     _inherited_slots: ClassVar[List[str]] = []
 
     class_class_uri: ClassVar[URIRef] = TEST_LINKML_PROJECT_COPIER["Person"]
@@ -151,7 +120,9 @@ class Person(NamedThing):
         if self.age_in_years is not None and not isinstance(self.age_in_years, int):
             self.age_in_years = int(self.age_in_years)
 
-        if self.vital_status is not None and not isinstance(self.vital_status, PersonStatus):
+        if self.vital_status is not None and not isinstance(
+            self.vital_status, PersonStatus
+        ):
             self.vital_status = PersonStatus(self.vital_status)
 
         super().__post_init__(**kwargs)
@@ -162,6 +133,7 @@ class PersonCollection(YAMLRoot):
     """
     A holder for Person objects
     """
+
     _inherited_slots: ClassVar[List[str]] = []
 
     class_class_uri: ClassVar[URIRef] = TEST_LINKML_PROJECT_COPIER["PersonCollection"]
@@ -169,61 +141,124 @@ class PersonCollection(YAMLRoot):
     class_name: ClassVar[str] = "PersonCollection"
     class_model_uri: ClassVar[URIRef] = TEST_LINKML_PROJECT_COPIER.PersonCollection
 
-    entries: Optional[Union[Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]]] = empty_dict()
+    entries: Optional[
+        Union[
+            Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]
+        ]
+    ] = empty_dict()
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        self._normalize_inlined_as_dict(slot_name="entries", slot_type=Person, key_name="id", keyed=True)
+        self._normalize_inlined_as_list(
+            slot_name="entries", slot_type=Person, key_name="id", keyed=True
+        )
 
         super().__post_init__(**kwargs)
 
 
 # Enumerations
 class PersonStatus(EnumDefinitionImpl):
-
     ALIVE = PermissibleValue(
-        text="ALIVE",
-        description="the person is living",
-        meaning=PATO["0001421"])
+        text="ALIVE", description="the person is living", meaning=PATO["0001421"]
+    )
     DEAD = PermissibleValue(
-        text="DEAD",
-        description="the person is deceased",
-        meaning=PATO["0001422"])
+        text="DEAD", description="the person is deceased", meaning=PATO["0001422"]
+    )
     UNKNOWN = PermissibleValue(
-        text="UNKNOWN",
-        description="the vital status is not known")
+        text="UNKNOWN", description="the vital status is not known"
+    )
 
     _defn = EnumDefinition(
         name="PersonStatus",
     )
 
+
 # Slots
 class slots:
     pass
 
-slots.id = Slot(uri=SCHEMA.identifier, name="id", curie=SCHEMA.curie('identifier'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.id, domain=None, range=URIRef)
 
-slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.name, domain=None, range=Optional[str])
+slots.id = Slot(
+    uri=SCHEMA.identifier,
+    name="id",
+    curie=SCHEMA.curie("identifier"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.id,
+    domain=None,
+    range=URIRef,
+)
 
-slots.description = Slot(uri=SCHEMA.description, name="description", curie=SCHEMA.curie('description'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.description, domain=None, range=Optional[str])
+slots.name = Slot(
+    uri=SCHEMA.name,
+    name="name",
+    curie=SCHEMA.curie("name"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.name,
+    domain=None,
+    range=Optional[str],
+)
 
-slots.primary_email = Slot(uri=SCHEMA.email, name="primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.primary_email, domain=None, range=Optional[str])
+slots.description = Slot(
+    uri=SCHEMA.description,
+    name="description",
+    curie=SCHEMA.curie("description"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.description,
+    domain=None,
+    range=Optional[str],
+)
 
-slots.birth_date = Slot(uri=SCHEMA.birthDate, name="birth_date", curie=SCHEMA.curie('birthDate'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.birth_date, domain=None, range=Optional[Union[str, XSDDate]])
+slots.primary_email = Slot(
+    uri=SCHEMA.email,
+    name="primary_email",
+    curie=SCHEMA.curie("email"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.primary_email,
+    domain=None,
+    range=Optional[str],
+)
 
-slots.age_in_years = Slot(uri=TEST_LINKML_PROJECT_COPIER.age_in_years, name="age_in_years", curie=TEST_LINKML_PROJECT_COPIER.curie('age_in_years'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.age_in_years, domain=None, range=Optional[int])
+slots.birth_date = Slot(
+    uri=SCHEMA.birthDate,
+    name="birth_date",
+    curie=SCHEMA.curie("birthDate"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.birth_date,
+    domain=None,
+    range=Optional[Union[str, XSDDate]],
+)
 
-slots.vital_status = Slot(uri=TEST_LINKML_PROJECT_COPIER.vital_status, name="vital_status", curie=TEST_LINKML_PROJECT_COPIER.curie('vital_status'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.vital_status, domain=None, range=Optional[Union[str, "PersonStatus"]])
+slots.age_in_years = Slot(
+    uri=TEST_LINKML_PROJECT_COPIER.age_in_years,
+    name="age_in_years",
+    curie=TEST_LINKML_PROJECT_COPIER.curie("age_in_years"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.age_in_years,
+    domain=None,
+    range=Optional[int],
+)
 
-slots.personCollection__entries = Slot(uri=TEST_LINKML_PROJECT_COPIER.entries, name="personCollection__entries", curie=TEST_LINKML_PROJECT_COPIER.curie('entries'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.personCollection__entries, domain=None, range=Optional[Union[Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]]])
+slots.vital_status = Slot(
+    uri=TEST_LINKML_PROJECT_COPIER.vital_status,
+    name="vital_status",
+    curie=TEST_LINKML_PROJECT_COPIER.curie("vital_status"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.vital_status,
+    domain=None,
+    range=Optional[Union[str, "PersonStatus"]],
+)
 
-slots.Person_primary_email = Slot(uri=SCHEMA.email, name="Person_primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=TEST_LINKML_PROJECT_COPIER.Person_primary_email, domain=Person, range=Optional[str],
-                   pattern=re.compile(r'^\S+@[\S+\.]+\S+'))
+slots.personCollection__entries = Slot(
+    uri=TEST_LINKML_PROJECT_COPIER.entries,
+    name="personCollection__entries",
+    curie=TEST_LINKML_PROJECT_COPIER.curie("entries"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.personCollection__entries,
+    domain=None,
+    range=Optional[
+        Union[
+            Dict[Union[str, PersonId], Union[dict, Person]], List[Union[dict, Person]]
+        ]
+    ],
+)
+
+slots.Person_primary_email = Slot(
+    uri=SCHEMA.email,
+    name="Person_primary_email",
+    curie=SCHEMA.curie("email"),
+    model_uri=TEST_LINKML_PROJECT_COPIER.Person_primary_email,
+    domain=Person,
+    range=Optional[str],
+    pattern=re.compile(r"^\S+@[\S+\.]+\S+"),
+)
--- src/test_linkml_project_copier/datamodel/test_linkml_project_copier_pydantic.py
+++ src/test_linkml_project_copier/datamodel/test_linkml_project_copier_pydantic.py
@@ -1,31 +1,11 @@
-from __future__ import annotations 
+from __future__ import annotations
 
 import re
-import sys
-from datetime import (
-    date,
-    datetime,
-    time
-)
-from decimal import Decimal 
-from enum import Enum 
-from typing import (
-    Any,
-    ClassVar,
-    Dict,
-    List,
-    Literal,
-    Optional,
-    Union
-)
+from datetime import date
+from enum import Enum
+from typing import Any, ClassVar, Dict, List, Optional
 
-from pydantic import (
-    BaseModel,
-    ConfigDict,
-    Field,
-    RootModel,
-    field_validator
-)
+from pydantic import BaseModel, ConfigDict, Field, RootModel, field_validator
 
 
 metamodel_version = "None"
@@ -34,57 +14,74 @@ version = "None"
 
 class ConfiguredBaseModel(BaseModel):
     model_config = ConfigDict(
-        validate_assignment = True,
-        validate_default = True,
-        extra = "forbid",
-        arbitrary_types_allowed = True,
-        use_enum_values = True,
-        strict = False,
+        validate_assignment=True,
+        validate_default=True,
+        extra="forbid",
+        arbitrary_types_allowed=True,
+        use_enum_values=True,
+        strict=False,
     )
     pass
-
-
 
 
 class LinkMLMeta(RootModel):
     root: Dict[str, Any] = {}
     model_config = ConfigDict(frozen=True)
 
-    def __getattr__(self, key:str):
+    def __getattr__(self, key: str):
         return getattr(self.root, key)
 
-    def __getitem__(self, key:str):
+    def __getitem__(self, key: str):
         return self.root[key]
 
-    def __setitem__(self, key:str, value):
+    def __setitem__(self, key: str, value):
         self.root[key] = value
 
-    def __contains__(self, key:str) -> bool:
+    def __contains__(self, key: str) -> bool:
         return key in self.root
 
 
-linkml_meta = LinkMLMeta({'default_prefix': 'test_linkml_project_copier',
-     'default_range': 'string',
-     'description': 'Test instance of linkml-copier-template.',
-     'id': 'https://w3id.org/dalito/test-linkml-project-copier',
-     'imports': ['linkml:types'],
-     'license': 'MIT',
-     'name': 'test-linkml-project-copier',
-     'prefixes': {'PATO': {'prefix_prefix': 'PATO',
-                           'prefix_reference': 'http://purl.obolibrary.org/obo/PATO_'},
-                  'biolink': {'prefix_prefix': 'biolink',
-                              'prefix_reference': 'https://w3id.org/biolink/'},
-                  'example': {'prefix_prefix': 'example',
-                              'prefix_reference': 'https://example.org/'},
-                  'linkml': {'prefix_prefix': 'linkml',
-                             'prefix_reference': 'https://w3id.org/linkml/'},
-                  'schema': {'prefix_prefix': 'schema',
-                             'prefix_reference': 'http://schema.org/'},
-                  'test_linkml_project_copier': {'prefix_prefix': 'test_linkml_project_copier',
-                                                 'prefix_reference': 'https://w3id.org/dalito/test-linkml-project-copier/'}},
-     'see_also': ['https://dalito.github.io/test-linkml-project-copier'],
-     'source_file': 'src/test_linkml_project_copier/schema/test_linkml_project_copier.yaml',
-     'title': 'test-linkml-project-copier'} )
+linkml_meta = LinkMLMeta(
+    {
+        "default_prefix": "test_linkml_project_copier",
+        "default_range": "string",
+        "description": "Test instance of linkml-copier-template.",
+        "id": "https://w3id.org/dalito/test-linkml-project-copier",
+        "imports": ["linkml:types"],
+        "license": "MIT",
+        "name": "test-linkml-project-copier",
+        "prefixes": {
+            "PATO": {
+                "prefix_prefix": "PATO",
+                "prefix_reference": "http://purl.obolibrary.org/obo/PATO_",
+            },
+            "biolink": {
+                "prefix_prefix": "biolink",
+                "prefix_reference": "https://w3id.org/biolink/",
+            },
+            "example": {
+                "prefix_prefix": "example",
+                "prefix_reference": "https://example.org/",
+            },
+            "linkml": {
+                "prefix_prefix": "linkml",
+                "prefix_reference": "https://w3id.org/linkml/",
+            },
+            "schema": {
+                "prefix_prefix": "schema",
+                "prefix_reference": "http://schema.org/",
+            },
+            "test_linkml_project_copier": {
+                "prefix_prefix": "test_linkml_project_copier",
+                "prefix_reference": "https://w3id.org/dalito/test-linkml-project-copier/",
+            },
+        },
+        "see_also": ["https://dalito.github.io/test-linkml-project-copier"],
+        "source_file": "src/test_linkml_project_copier/schema/test_linkml_project_copier.yaml",
+        "title": "test-linkml-project-copier",
+    }
+)
+
 
 class PersonStatus(str, Enum):
     # the person is living
@@ -95,47 +92,148 @@ class PersonStatus(str, Enum):
     UNKNOWN = "UNKNOWN"
 
 
-
 class NamedThing(ConfiguredBaseModel):
     """
     A generic grouping for any identifiable entity
     """
-    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta({'class_uri': 'schema:Thing',
-         'from_schema': 'https://w3id.org/dalito/test-linkml-project-copier'})
 
-    id: str = Field(default=..., description="""A unique identifier for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'id', 'domain_of': ['NamedThing'], 'slot_uri': 'schema:identifier'} })
-    name: Optional[str] = Field(default=None, description="""A human-readable name for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'name', 'domain_of': ['NamedThing'], 'slot_uri': 'schema:name'} })
-    description: Optional[str] = Field(default=None, description="""A human-readable description for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'description',
-         'domain_of': ['NamedThing'],
-         'slot_uri': 'schema:description'} })
+    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta(
+        {
+            "class_uri": "schema:Thing",
+            "from_schema": "https://w3id.org/dalito/test-linkml-project-copier",
+        }
+    )
+
+    id: str = Field(
+        default=...,
+        description="""A unique identifier for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "id",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:identifier",
+            }
+        },
+    )
+    name: Optional[str] = Field(
+        default=None,
+        description="""A human-readable name for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "name",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:name",
+            }
+        },
+    )
+    description: Optional[str] = Field(
+        default=None,
+        description="""A human-readable description for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "description",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:description",
+            }
+        },
+    )
 
 
 class Person(NamedThing):
     """
     Represents a Person
     """
-    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta({'from_schema': 'https://w3id.org/dalito/test-linkml-project-copier',
-         'slot_usage': {'primary_email': {'name': 'primary_email',
-                                          'pattern': '^\\S+@[\\S+\\.]+\\S+'}}})
 
-    primary_email: Optional[str] = Field(default=None, description="""The main email address of a person""", json_schema_extra = { "linkml_meta": {'alias': 'primary_email', 'domain_of': ['Person'], 'slot_uri': 'schema:email'} })
-    birth_date: Optional[date] = Field(default=None, description="""Date on which a person is born""", json_schema_extra = { "linkml_meta": {'alias': 'birth_date', 'domain_of': ['Person'], 'slot_uri': 'schema:birthDate'} })
-    age_in_years: Optional[int] = Field(default=None, description="""Number of years since birth""", json_schema_extra = { "linkml_meta": {'alias': 'age_in_years', 'domain_of': ['Person']} })
-    vital_status: Optional[PersonStatus] = Field(default=None, description="""living or dead status""", json_schema_extra = { "linkml_meta": {'alias': 'vital_status', 'domain_of': ['Person']} })
-    id: str = Field(default=..., description="""A unique identifier for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'id', 'domain_of': ['NamedThing'], 'slot_uri': 'schema:identifier'} })
-    name: Optional[str] = Field(default=None, description="""A human-readable name for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'name', 'domain_of': ['NamedThing'], 'slot_uri': 'schema:name'} })
-    description: Optional[str] = Field(default=None, description="""A human-readable description for a thing""", json_schema_extra = { "linkml_meta": {'alias': 'description',
-         'domain_of': ['NamedThing'],
-         'slot_uri': 'schema:description'} })
+    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta(
+        {
+            "from_schema": "https://w3id.org/dalito/test-linkml-project-copier",
+            "slot_usage": {
+                "primary_email": {
+                    "name": "primary_email",
+                    "pattern": "^\\S+@[\\S+\\.]+\\S+",
+                }
+            },
+        }
+    )
 
-    @field_validator('primary_email')
+    primary_email: Optional[str] = Field(
+        default=None,
+        description="""The main email address of a person""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "primary_email",
+                "domain_of": ["Person"],
+                "slot_uri": "schema:email",
+            }
+        },
+    )
+    birth_date: Optional[date] = Field(
+        default=None,
+        description="""Date on which a person is born""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "birth_date",
+                "domain_of": ["Person"],
+                "slot_uri": "schema:birthDate",
+            }
+        },
+    )
+    age_in_years: Optional[int] = Field(
+        default=None,
+        description="""Number of years since birth""",
+        json_schema_extra={
+            "linkml_meta": {"alias": "age_in_years", "domain_of": ["Person"]}
+        },
+    )
+    vital_status: Optional[PersonStatus] = Field(
+        default=None,
+        description="""living or dead status""",
+        json_schema_extra={
+            "linkml_meta": {"alias": "vital_status", "domain_of": ["Person"]}
+        },
+    )
+    id: str = Field(
+        default=...,
+        description="""A unique identifier for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "id",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:identifier",
+            }
+        },
+    )
+    name: Optional[str] = Field(
+        default=None,
+        description="""A human-readable name for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "name",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:name",
+            }
+        },
+    )
+    description: Optional[str] = Field(
+        default=None,
+        description="""A human-readable description for a thing""",
+        json_schema_extra={
+            "linkml_meta": {
+                "alias": "description",
+                "domain_of": ["NamedThing"],
+                "slot_uri": "schema:description",
+            }
+        },
+    )
+
+    @field_validator("primary_email")
     def pattern_primary_email(cls, v):
-        pattern=re.compile(r"^\S+@[\S+\.]+\S+")
-        if isinstance(v,list):
+        pattern = re.compile(r"^\S+@[\S+\.]+\S+")
+        if isinstance(v, list):
             for element in v:
                 if isinstance(v, str) and not pattern.match(element):
                     raise ValueError(f"Invalid primary_email format: {element}")
-        elif isinstance(v,str):
+        elif isinstance(v, str):
             if not pattern.match(v):
                 raise ValueError(f"Invalid primary_email format: {v}")
         return v
@@ -145,10 +243,20 @@ class PersonCollection(ConfiguredBaseModel):
     """
     A holder for Person objects
     """
-    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta({'from_schema': 'https://w3id.org/dalito/test-linkml-project-copier',
-         'tree_root': True})
 
-    entries: Optional[Dict[str, Person]] = Field(default=None, json_schema_extra = { "linkml_meta": {'alias': 'entries', 'domain_of': ['PersonCollection']} })
+    linkml_meta: ClassVar[LinkMLMeta] = LinkMLMeta(
+        {
+            "from_schema": "https://w3id.org/dalito/test-linkml-project-copier",
+            "tree_root": True,
+        }
+    )
+
+    entries: Optional[List[Person]] = Field(
+        default=None,
+        json_schema_extra={
+            "linkml_meta": {"alias": "entries", "domain_of": ["PersonCollection"]}
+        },
+    )
 
 
 # Model rebuild
@@ -156,4 +264,3 @@ class PersonCollection(ConfiguredBaseModel):
 NamedThing.model_rebuild()
 Person.model_rebuild()
 PersonCollection.model_rebuild()
-
